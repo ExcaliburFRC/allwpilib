800f57b6101c51eb285e0cd5c496bb6e4c5e48dc
Prateek Machiraju <prateek.machiraju@gmail.com>
2022-06-02 20:04:18 -0700

    [wpilibc] Use GetBatteryVoltage() in MotorController::SetVoltage
    This avoids having to cast to units::voltage_t

--- wpilibc/src/main/native/cpp/SpeedController.cpp
+++ wpilibc/src/main/native/cpp/SpeedController.cpp
@@ -8,6 +8,6 @@
 
 using namespace frc;
 
-void SpeedController::SetVoltage(units::volt_t output) {
-  Set(output / units::volt_t(RobotController::GetInputVoltage()));
+void MotorController::SetVoltage(units::volt_t output) {
+  Set(output / RobotController::GetBatteryVoltage());
 }
